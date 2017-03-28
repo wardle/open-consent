5e3b44646434f27293b1b7b400dd6361c2fe0549
Mark Wardle <mark@wardle.org>
2017-03-28 14:03:30 +0100

    Run bootique on startup.

--- src/main/java/com/eldrix/openconsent/Application.java
+++ src/main/java/com/eldrix/openconsent/Application.java
@@ -1,13 +1,14 @@
 package com.eldrix.openconsent;
 
+import io.bootique.Bootique;
+
 /**
  * Hello world!
  *
  */
-public class Application 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Application {
+    public static void main( String[] args ) {
+        System.out.println("OpenConsent server");
+		Bootique.app(args).autoLoadModules().run();
     }
 }
