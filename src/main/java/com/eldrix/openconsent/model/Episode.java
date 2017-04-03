8ce2f4f0c294331e4a67ca0ad48aa80629ee6ad8
Mark Wardle <mark@wardle.org>
2017-04-03 16:47:55 +0100

    Add an episode entity to track a project or service’s patients.

---
+++ src/main/java/com/eldrix/openconsent/model/Episode.java
@@ -0,0 +1,9 @@
+package com.eldrix.openconsent.model;
+
+import com.eldrix.openconsent.model.auto._Episode;
+
+public class Episode extends _Episode {
+
+    private static final long serialVersionUID = 1L; 
+
+}
