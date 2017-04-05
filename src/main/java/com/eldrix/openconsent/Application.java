aa83549f6ca350252c03050b11652d9a545304aa
Mark Wardle <mark@wardle.org>
2017-04-05 19:41:37 +0100

    Make application an injectable module.
    Next steps, to add JAX-RS resources in the configuration.

--- src/main/java/com/eldrix/openconsent/Application.java
+++ src/main/java/com/eldrix/openconsent/Application.java
@@ -1,14 +1,24 @@
 package com.eldrix.openconsent;
 
-import io.bootique.Bootique;
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.multibindings.Multibinder;
 
-/**
- * Hello world!
- *
- */
-public class Application {
+import io.bootique.Bootique;
+import io.bootique.jersey.JerseyModule;
+
+public class Application implements Module {
     public static void main( String[] args ) {
         System.out.println("OpenConsent server");
-		Bootique.app(args).autoLoadModules().run();
+		Bootique.app(args)
+			.module(Application.class)
+			.autoLoadModules()
+			.run();
     }
+    
+	@Override
+	public void configure(Binder binder) {
+		Multibinder<Object> jersey = JerseyModule.contributeResources(binder);
+		//jersey.addBinding().to(Resource.class);
+	}
 }
