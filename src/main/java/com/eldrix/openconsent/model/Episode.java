a1b15abe96b8f69c9688ccb1a5df3390ad01a0ab
Mark Wardle <mark@wardle.org>
2017-04-09 07:04:25 +0100

    Remove unneeded method.

--- src/main/java/com/eldrix/openconsent/model/Episode.java
+++ src/main/java/com/eldrix/openconsent/model/Episode.java
@@ -1,6 +1,5 @@
 package com.eldrix.openconsent.model;
 
-import org.apache.cayenne.validation.ValidationResult;
 
 import com.eldrix.openconsent.model.auto._Episode;
 
@@ -8,8 +7,4 @@ public class Episode extends _Episode {
 
     private static final long serialVersionUID = 1L; 
 
-    @Override
-    protected void validateForSave(ValidationResult validationResult) {
-    	super.validateForSave(validationResult);
-    }
 }
